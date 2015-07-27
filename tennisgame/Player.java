bf9c52ddb451f799a78e5a772309df3c3ceea524
manish <manishengi25@gmail.com>
2015-07-27 11:15:04 +0530

    Player.java  file is added

---
+++ tennisgame/Player.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tennisgame;
+
+/**
+ *
+ * @author test
+ */
+public class Player {
+
+   
+    private String playerName;
+
+    public void setPlayerName( String name) {
+        playerName = name;
+    }
+}
